51f001f7bb506b70980c54c347c1b62320e2ca0f
syifasalsabyla228@gmail.com <syifasalsabyla228@gmail.com>
2019-03-06 09:07:38 +0700

    Add project files.

---
+++ latihanLogSig9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace latihanLogSig9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
